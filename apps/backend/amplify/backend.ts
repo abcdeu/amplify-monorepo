7b75e415a6c2756dece562fc50f81f54539933b6
ao <pcngt903@gmail.com>
2025-09-11 02:11:38 +0900

    enabled db pointintime recovery

--- apps/backend/amplify/backend.ts
+++ apps/backend/amplify/backend.ts
@@ -5,7 +5,12 @@ import { data } from './data/resource';
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+const { amplifyDynamoDbTables } = backend.data.resources.cfnResources;
+for (const table of Object.values(amplifyDynamoDbTables)) {
+  table.pointInTimeRecoveryEnabled = true;
+}
