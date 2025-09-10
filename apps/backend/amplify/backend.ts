0595e52befec0d0f58b5d32bf0ec364144627aed
ao <pcngt903@gmail.com>
2025-09-11 02:51:12 +0900

    add backups

--- apps/backend/amplify/backend.ts
+++ apps/backend/amplify/backend.ts
@@ -2,6 +2,14 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
+import {
+  BackupPlan,
+  BackupPlanRule,
+  BackupResource,
+  BackupVault,
+} from "aws-cdk-lib/aws-backup";
+import { Schedule } from "aws-cdk-lib/aws-events";
+import { Duration } from "aws-cdk-lib/core";
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
@@ -14,3 +22,38 @@ const { amplifyDynamoDbTables } = backend.data.resources.cfnResources;
 for (const table of Object.values(amplifyDynamoDbTables)) {
   table.pointInTimeRecoveryEnabled = true;
 }
+
+const backupStack = backend.createStack("backup-stack");
+const myTables = Object.values(backend.data.resources.tables);
+
+
+const vault = new BackupVault(backupStack, "BackupVault", {
+  backupVaultName: "backup-vault",
+});
+
+
+const plan = new BackupPlan(backupStack, "BackupPlan", {
+  backupPlanName: "backup-plan",
+  backupVault: vault,
+});
+
+
+plan.addRule(
+  new BackupPlanRule({
+    deleteAfter: Duration.days(60),
+    ruleName: "backup-plan-rule",
+    scheduleExpression: Schedule.cron({
+      minute: "0",
+      hour: "0",
+      day: "*",
+      month: "*",
+      year: "*",
+    }),
+  })
+);
+
+
+plan.addSelection("BackupPlanSelection", {
+  resources: myTables.map((table) => BackupResource.fromDynamoDbTable(table)),
+  allowRestores: true,
+});
